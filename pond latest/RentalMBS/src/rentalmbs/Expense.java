6c5dce3f88996c2a73aae76d4c3f92416297c6a9
Sahussawud Khunruksa <43010244+sahussawud@users.noreply.github.com>
2019-11-24 19:39:54 +0700

    update finish dashboard

--- pond latest/RentalMBS/src/rentalmbs/Expense.java
+++ pond latest/RentalMBS/src/rentalmbs/Expense.java
@@ -1,7 +1,15 @@
 package rentalmbs;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 import rentalmbs.Database;
 
@@ -17,6 +25,7 @@ import rentalmbs.Database;
  */
 public class Expense {
     JTable ExpenseTB;
+    
     String ExpenseID,ExpType,payee,detail;
     Date ExpenseDate;
     double cost;
@@ -52,6 +61,7 @@ public class Expense {
     }
     public void setExpenseTB(JTable ExpenseTB) {
         this.ExpenseTB = ExpenseTB;
+        
     }
     public String getExpenseID() {
         return ExpenseID;
@@ -92,7 +102,51 @@ public class Expense {
 
    
  
- 
+    public void addExpense(){
+     if (IDexpense.getText().equals("")) {
+            JOptionPane.showMessageDialog(this, "กรุณากรอกข้อมูลให้ครบทุกช่อง");
+        } else {// TODO  add your handling code here:
+            String Id = IDexpense.getText();
+
+            SimpleDateFormat Date_Format = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+            String Day = Date_Format.format(dateeeee.getDate());
+
+            String type = Type.getSelectedItem().toString();
+            String Reciver = reciever.getText();
+            String Amount = amount_exp.getText();
+            String Detail = detail.getText();
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://ihost.it.kmitl.ac.th:3306/it61070203_base?useUnicode=true&characterEncoding=utf-8", "it61070203_base", "123456");
+                Statement stmt = con.createStatement();
+                PreparedStatement pst;
+                pst = con.prepareStatement("insert into ExpenseTB(id_expense,date_expense,type_expense,reciever_expense,amount_expense,detail_expense,least_editor) values(?,?,?,?,?,?,?)");
+                pst.setString(1, Id);
+                pst.setString(2, Day);
+                pst.setString(3, type);
+                pst.setString(4, Reciver);
+                pst.setString(5, Amount);
+                pst.setString(6, Detail);
+                pst.setString(7, this.user);
+
+                pst.executeUpdate();
+                JOptionPane.showMessageDialog(this, "Car Added.......");
+
+                IDexpense.setText("");
+                //jDateChooser1.setDate(null);
+                Type.setSelectedIndex(-1);
+                reciever.setText("");
+                amount_exp.setText("");
+                detail.setText("");
+                table_update3();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(carregis.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(carregis.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     
 
 
